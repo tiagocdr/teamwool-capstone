c8837035b84bdb58f4a33bbd89298b1d4cb4b876
Joseph Dubon <josephdubon@pm.me>
2021-04-05 11:51:37 -0700

    Implemented environment variables to lock-in secret-key

--- woolnews_config/settings.py
+++ woolnews_config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_u+fhz*ou&7&3frr(qfrhyl1#@%lg=xm+#-hrn*38x4+niw^&7'
+SECRET_KEY = os.environ.get(
+    'DJANGO_SECRET',
+    '_u+fhz*ou&7&3frr(qfrhyl1#@%lg=xm+#-hrn*38x4+niw^&7'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
