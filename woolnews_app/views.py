5c2b2fd2e3ed380a6437e74d19a131488c9feb00
Joseph Dubon <josephdubon@pm.me>
2021-04-07 12:59:57 -0700

    Added TEMP views for: home, forum, about, contact

--- woolnews_app/views.py
+++ woolnews_app/views.py
@@ -2,3 +2,38 @@ from django.shortcuts import render
 
 
 # TODO: Post View
+
+# TEMP Home/News list view
+def home_view(request):
+    return render(
+        request,
+        'news.html',
+        {}
+    )
+
+
+# TEMP Forum list view
+def forum_view(request):
+    return render(
+        request,
+        'forum.html',
+        {}
+    )
+
+
+# TEMP About view
+def about_view(request):
+    return render(
+        request,
+        'about.html',
+        {}
+    )
+
+
+# TEMP Contact view
+def contact_view(request):
+    return render(
+        request,
+        'contact.html',
+        {}
+    )
