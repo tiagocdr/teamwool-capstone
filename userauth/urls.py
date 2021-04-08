91a6647b7b9ebcaf273b9c2bc311f3e721a7afe6
Eleanor Hall <ruthhall97@gmail.com>
2021-04-07 21:37:59 -0700

    editprofile/changepassword/elly

--- userauth/urls.py
+++ userauth/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from userauth import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
-    path('edit_profile/', views.UserEditView.as_view(), name='edit_profile')
+    path('edit_profile/', views.UserEditView.as_view(), name='edit_profile'),
+    path('password/', auth_views.PasswordChangeView.as_view()),
 ]
--- userauth/views.py
+++ userauth/views.py
@@ -1,16 +1,20 @@
 
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
-from .forms import CustomUserCreationForm
+from django.views import generic
+from .forms import CustomUserCreationForm, EditProfileForm
 from django.contrib.auth.forms import  UserChangeForm
 
 # SignUp View
-class SignUpView(CreateView):
+class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
-class UserEditView(CreateView):
-    form_class = UserChangeForm
+
+class UserEditView(generic.UpdateView):
+    form_class = EditProfileForm
     success_url = reverse_lazy('home')
     template_name = 'edit_profile.html'
+    
+    def get_object(self):
+        return self.request.user
