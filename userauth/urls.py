6ac71d4d2ce160e2359ddc93a9511b48a16d3b5c
Eleanor Hall <ruthhall97@gmail.com>
2021-04-07 21:47:00 -0700

    updatepassword-page added a html file name update password  /elly

--- userauth/urls.py
+++ userauth/urls.py
@@ -5,5 +5,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('edit_profile/', views.UserEditView.as_view(), name='edit_profile'),
-    path('password/', auth_views.PasswordChangeView.as_view()),
+    path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/update-password.html')),
 ]
