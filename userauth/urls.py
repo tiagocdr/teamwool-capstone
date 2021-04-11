b970f41e5ecc133a5ac58068f06d098e6d95b160
santi <tiagoelcoder@gmail.com>
2021-04-11 11:27:25 -0400

    Connected user urls

--- userauth/urls.py
+++ userauth/urls.py
@@ -4,6 +4,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('profile/<int:user_id>', views.user_profile, name='profile'),
     path('edit_profile/', views.UserEditView.as_view(), name='edit_profile'),
     path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/update-password.html')),
 ]
--- woolnews_config/urls.py
+++ woolnews_config/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from woolnews_app.views import HomeView, DiscussionView, AboutView, ContactView
 from woolnews_app import views
 from django.conf import settings
 from django.conf.urls.static import static
+from userauth.urls import urlpatterns as user_urls
 
 
 urlpatterns = [
@@ -35,6 +37,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
 ] 
 
+urlpatterns = urlpatterns + user_urls
 if settings.DEBUG:
         urlpatterns += static(settings.MEDIA_URL,
                               document_root=settings.MEDIA_ROOT)
