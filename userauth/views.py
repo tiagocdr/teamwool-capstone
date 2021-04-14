50798fa5144f6bb4fcd5c4450c53e1a3add981a2
santi <tiagoelcoder@gmail.com>
2021-04-13 23:56:38 -0400

    Changed detail view to template view, Added saved items and posts to profile page

--- userauth/views.py
+++ userauth/views.py
@@ -1,5 +1,7 @@
 from django.views.generic.detail import DetailView
+from django.views.generic import TemplateView
 from woolnews_app.models import PostModel
+from favorites.models import FavoritesModel
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import CustomUserCreationForm, EditProfileForm
@@ -28,17 +30,23 @@ class PasswordsChangeView(PasswordChangeView):
     form_class = PasswordChangeForm
     success_url = reverse_lazy('home')
     
-class ProfileView(DetailView):
+class ProfileView(TemplateView):
     model = CustomUser
     template_name = 'registration/profile.html'
-    
+
     def get_queryset(self):
         print(self.kwargs)
-        self.User=CustomUser.objects.filter(id=self.kwargs["pk"])
+        self.User=CustomUser.objects.get(id=self.kwargs["pk"])
         return self.User
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["user"]= self.User
+        user = self.get_queryset()
+        context["user"]= user
+        context["own_posts"] = PostModel.objects.filter(user=user)
+        context['saved'] = FavoritesModel.objects.filter(user=user)
+        return context
+
     
   
 class UserEditView(generic.UpdateView):
