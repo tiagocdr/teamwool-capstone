23169f3a9aac7c9942ba311f51ea4a6b5353ead1
santi <tiagoelcoder@gmail.com>
2021-04-22 11:53:01 -0400

    deployment settings

--- woolnews_config/settings.py
+++ woolnews_config/settings.py
@@ -146,3 +146,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 # STATIFILES_STORAGE 
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
